72b0953964c8c6504b462d026ad625fbd74e1667
hasansendani <h.sendani@digikala.com>
2023-07-04 23:22:12 +0330

    delete unnecessary field devlerations to free memory

--- MiniJava/src/main/java/codeGenerator/Memory.java
+++ MiniJava/src/main/java/codeGenerator/Memory.java
@@ -9,25 +9,21 @@ public class Memory {
     private ArrayList<_3AddressCode> codeBlock;
     private int lastTempIndex;
     private int lastDataAddress;
-    private final int stratTempMemoryAddress = 500;
-    private final int stratDataMemoryAddress = 200;
-    private final int dataSize = 4;
-    private final int tempSize = 4;
 
     public Memory() {
         codeBlock = new ArrayList<_3AddressCode>();
-        lastTempIndex = stratTempMemoryAddress;
-        lastDataAddress = stratDataMemoryAddress;
+        lastTempIndex = 500;
+        lastDataAddress = 200;
     }
 
     public int getTemp() {
-        lastTempIndex += tempSize;
-        return lastTempIndex - tempSize;
+        lastTempIndex += 4;
+        return lastTempIndex - 4;
     }
 
     public int getDateAddress() {
-        lastDataAddress += dataSize;
-        return lastDataAddress - dataSize;
+        lastDataAddress += 4;
+        return lastDataAddress - 4;
     }
 
     public int saveMemory() {
